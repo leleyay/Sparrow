a42dec512c8d026232b46df4498b001f912c35eb
lionet <3339496031@qq.com>
2022-09-11 00:30:40 +0800

    feat: create renderer

---
+++ src/renderer/renderer.js
@@ -0,0 +1,27 @@
+import { createContext } from './context';
+import {
+  circle, line, text, rect, path, ring,
+} from './shape';
+import {
+  restore, rotate, save, scale, translate,
+} from './transform';
+
+export function createRenderer(width, height) {
+  const context = createContext(width, height);
+
+  return {
+    line: (options) => line(context, options),
+    circle: (options) => circle(context, options),
+    text: (options) => text(context, options),
+    rect: (options) => rect(context, options),
+    path: (options) => path(context, options),
+    ring: (options) => ring(context, options),
+    restore: () => restore(context),
+    save: () => save(context),
+    scale: (...args) => scale(context, ...args),
+    rotate: (...args) => rotate(context, ...args),
+    translate: (...args) => translate(context, ...args),
+    node: () => context.node,
+    group: () => context.group,
+  };
+}
